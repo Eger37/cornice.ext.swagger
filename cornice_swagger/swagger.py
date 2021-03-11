5baf1ec58da122c29c338889071b50d5f551a87e
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2021-03-11 15:43:42 -0500

    add support of recursive definition schema references in body parameter

--- cornice_swagger/swagger.py
+++ cornice_swagger/swagger.py
@@ -215,11 +215,18 @@ class ParameterHandler(object):
                 name = param_schema.__class__.__name__
                 if name == 'body':
                     name = schema_node.__class__.__name__ + 'Body'
-                param = self.parameter_converter(location,
-                                                 param_schema)
+                param = self.parameter_converter(location, param_schema)
                 param['name'] = name
                 if self.ref:
                     param = self._ref(param)
+                    # if definitions reference depth was also requested,
+                    # loop down body elements and replace the parameter reference
+                    if self.definitions.ref:
+                        body_ref = self.definitions.from_schema(param_schema, name)
+                        body_schema = self.definitions.definition_registry.get(name)
+                        param_ref = self.parameter_registry.get(name)
+                        param_ref["schema"] = body_ref
+
                 params.append(param)
 
             elif location in (('path', 'header', 'headers', 'querystring', 'GET')):
